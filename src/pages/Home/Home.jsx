206d89e6c0773bfa37a913bf994b45c9b4ec940a
RenatoFerreira <renatoyferreira@gmail.com>
2024-01-02 20:25:36 -0300

    create: basic info

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import {MainScreen, BasicInfo, TransactionInfo, MarketInfo } from "./Style";
+import {MainScreen, BasicInfo, TransactionInfo, MarketInfo, CardBasic } from "./Style";
 import ScreenWithBars from "../../components/ScreenWithBars/ScreenWithBars";
 
 export default function Home() {
@@ -6,7 +6,18 @@ export default function Home() {
         <ScreenWithBars>
             <MainScreen>
                 <BasicInfo>
-                    {/* 3 informacões: Total/ A pagar / Investimentos */}
+                    <CardBasic color={"linear-gradient(90deg, rgb(36, 59, 85), rgb(20, 30, 48) 100%)"}>
+                        <h1>Dinheiro na Carteira</h1>
+                        <span>R$ 4.202,30</span>
+                    </CardBasic>
+                    <CardBasic color={"linear-gradient(90deg, rgba(85,36,36,1) 35%, rgba(48,20,20,1) 100%)"}>
+                        <h1>Dívidas à vencer</h1>
+                        <span>R$ 8.765,03</span>
+                    </CardBasic>
+                    <CardBasic color={"linear-gradient(90deg, rgba(36,85,42,1) 35%, rgba(20,48,23,1) 100%)"}>
+                        <h1>Investimentos </h1>
+                        <span>R$ 32.123,11</span>
+                    </CardBasic>
                 </BasicInfo>
                 <MarketInfo>
                     {/* CDI IPCA E SELIC DO DIA */}
--- src/pages/Home/Style.jsx
+++ src/pages/Home/Style.jsx
@@ -4,12 +4,10 @@ export const MainScreen = styled.div`
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
+    margin-left: 70px;
 `
 export const BasicInfo = styled.div`
-    border: 1px solid blue;
-    width: 500px;
-    height: 300px;
-    display: block;
+    display: flex;
 `
 export const TransactionInfo = styled.div`
     border: 1px solid blue;
@@ -23,4 +21,33 @@ export const MarketInfo = styled.div`
     width: 500px;
     height: 300px;
     display: block;
+`
+
+export const CardBasic = styled.div`
+    width: 250px;
+    height: 130px;
+    background: rgb(36,48,85);
+    background: ${props => props.color } ;
+    box-shadow: rgb(0,0,0,0.7) 5px 5px 5px ,rgb(0,0,0,0.7) -5px 0px 250px;
+    margin: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    border-radius: 15px;
+    &>span{
+        font-size: 25px;
+        font-weight: 700;
+        color: #fff;
+    }
+    &>h1{
+        width: 100%;
+        color: #fff;
+        position: absolute;
+        top: 10px;
+        left: 10px;
+        font-size: 14px;
+        font-weight: 700;
+    }
 `
